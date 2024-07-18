2a0c122f86bef4301c2cc91f49979d3e8324a88c
Vasav Juyal <vasavj123@gmail.com>
2024-07-19 00:48:43 +0530

    Edit functionality complete, model and camera ability left. Style changes still needed

--- face-recognition-app/app.py
+++ face-recognition-app/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory, jsonify, flash, get_flashed_messages
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.utils import secure_filename
 import os
 import uuid
-from db import get_db, add_user, add_employee, get_user, init_db, get_all_employees, get_employee, get_employee_one_name, delete_employee
+from db import get_db, add_user, add_employee, get_user, init_db, get_all_employees, get_employee, get_employee_one_name, delete_employee, get_user_by_id, get_employee_by_id, edit_employee_image,edit_employee_no_image
 
 app = Flask(__name__)
 app.secret_key = 'test123'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 UPLOAD_FOLDER = 'uploads'
@@ -18,7 +23,13 @@ if not os.path.exists(UPLOAD_FOLDER):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@login_manager.user_loader
+def load_user(user_id):
+    with get_db() as db_:
+        return get_user_by_id(db_, user_id)
+
 @app.route('/uploads/<filename>')
+@login_required
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
@@ -31,16 +42,18 @@ def validate():
     username = request.form['username']
     password = request.form['password']
     
-    db_ = next(get_db())
-    user = get_user(db_, username)
+    with get_db() as db_:
+        user = get_user(db_, username)
     
-    if user is not None:
-        if user.username == username and user.password == password:
-            session['username'] = username
-            session['id'] = user.id
-            return redirect(url_for('home'))        
-    else:
-        return redirect(url_for('login'))
+        if user is not None:
+            if user.username == username and user.password == password:
+                session['username'] = username
+                session['id'] = user.id
+                login_user(user)
+                return redirect(url_for('home'))        
+        else:
+            flash('Invalid username or password', 'error')
+            return redirect(url_for('login'))
     
 @app.route('/register')
 def register():
@@ -51,49 +64,47 @@ def registrate():
     username = request.form['username']
     password = request.form['password']
     
-    db_ = next(get_db())
-    user = get_user(db_, username)
+    with get_db() as db_:
+        user = get_user(db_, username)
     
-    if user is None:      
-        session['username'] = username
-        add_user(db_, username, password)
+        if user is None:      
+            session['username'] = username
+            add_user(db_, username, password)
         
-        user_ = get_user(db_, username)
-        session['id'] = user_.id
-                
-        return redirect(url_for('home'))        
-    else:
-        return redirect(url_for('register'))
+            user_ = get_user(db_, username)
+            session['id'] = user_.id
+            login_user(user)        
+            return redirect(url_for('home'))        
+        else:
+            return redirect(url_for('register'))
     
 @app.route('/')
+@login_required
 def home():
-    if 'username' in session:
-        return render_template('dashboard.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('dashboard.html')
 
 @app.route('/attendance')
+@login_required
 def attendance():
-    if 'username' in session:
-        return render_template('attendance.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('attendance.html')
     
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('username', None)
+    session.pop('id', None)
+    logout_user()
     return redirect(url_for('login'))
-
+    
 
 
 @app.route('/add-new')
+@login_required
 def add() :
-    if 'username' in session:
-        return render_template('add.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('add.html')
     
 @app.route('/add-new/add', methods=['POST'])
+@login_required
 def upload_file():
     if 'image' not in request.files:
         print("No image")
@@ -112,8 +123,8 @@ def upload_file():
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         id = session['id']
-        db = next(get_db())
-        add_employee(db, firstname, lastname, filename, id)
+        with get_db() as db_:
+            add_employee(db_, firstname, lastname, filename, id)
         
         print('File successfully uploaded')
         return redirect(url_for('add'))
@@ -123,44 +134,69 @@ def upload_file():
 
 
 @app.route('/edit')
+@login_required
 def edit() :
-    if 'username' in session:
-        if request.args.get('search'):
-            search_result = request.args.get('search')
-            name_list = search_result.split()
-            db_ = next(get_db())
-    
+    if request.args.get('search'):
+        search_result = request.args.get('search')
+        name_list = search_result.split()
+        
+        with get_db() as db_:
             if len(name_list) == 1:
                 emps = get_employee_one_name(db_, name_list[0], session['id'])
             else: 
                 emps = get_employee(db_, name_list[0], name_list[1], session['id'])
-        else:
-            db_ = next(get_db())
-            emps = get_all_employees(db_, session['id'])
-            emps_json = [emp.to_dict() for emp in emps]
-         
-        return render_template('edit.html', emps=emps_json)
     else:
-        return redirect(url_for('login'))
+        with get_db() as db_:
+            emps = get_all_employees(db_, session['id'])
+            
+    emps_json = [emp.to_dict() for emp in emps]        
+    return render_template('edit.html', emps=emps_json)
 
 @app.route('/edit/delete', methods=['POST'])
+@login_required
 def delete():
     emp_id = request.json
-    db_ = next(get_db())
-    emp = delete_employee(db_, emp_id)
-    if emp:
-        return jsonify(emp)
-    else:
-        return {"status": "error"}
+    with get_db() as db_:
+        emp = delete_employee(db_, emp_id)
+        if emp:
+            return jsonify(emp)
+        else:
+            return {"status": "error"}
     
+@app.route('/edit/<int:employee_id>')
+@login_required
+def edit_one(employee_id):
+    with get_db() as db_:
+        emp = get_employee_by_id(db_, employee_id)
     
+        if emp and emp.controller_id == current_user.id:
+            return render_template('edit-one.html', emp=emp)
+        else:
+            return redirect(url_for('edit'))
+   
+@app.route('/edit/info', methods=['POST'])
+@login_required
+def edit_one_handler():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    id = request.form.get('id')
+    file = request.files['image']
+    
+    with get_db() as db_: 
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            edit_employee_image(db_,id, firstname, lastname, filename)        
+        else:
+            edit_employee_no_image(db_, id, firstname, lastname)
+    return redirect(url_for('edit'))
+       
     
 @app.route('/record-attendance')
+@login_required
 def record():
-    if 'username' in session:
-        return render_template('record.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('record.html')
 
     
 if __name__ == '__main__':
--- face-recognition-app/db.py
+++ face-recognition-app/db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, func, or_, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from flask_login import UserMixin
+from contextlib import contextmanager
 
 DATABASE_URL = "sqlite:///admin.db"
 Base = declarative_base()
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = 'admins'
 
     id = Column(Integer, primary_key=True, index=True)
@@ -13,7 +15,7 @@ class User(Base):
     password = Column(String, index=True)
     employees = relationship('Employee', back_populates='controller')
 
-class Employee(Base):
+class Employee(Base, UserMixin):
     __tablename__ = 'employees'
     id = Column(Integer, primary_key=True, autoincrement=True)
     firstname = Column(String,  nullable=False)
@@ -37,6 +39,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def init_db():
     Base.metadata.create_all(bind=engine)
 
+@contextmanager
 def get_db():
     db = SessionLocal()
     try:
@@ -62,8 +65,14 @@ def add_employee(db, firstname, lastname,image_path, controller_id):
 def get_user(db, username):
     return db.query(User).filter(User.username == username).first()
 
+def get_user_by_id(db, id):
+    return db.query(User).filter(User.id == id).first()
+
 def get_all_employees(db, ctr_id):
     return db.query(Employee).filter(Employee.controller_id == ctr_id).all()
+
+def get_employee_by_id(db, id):
+    return db.query(Employee).filter(Employee.id == id).first()
 
 def delete_employee(db, id):
     emp = db.query(Employee).filter(Employee.id == id).first()
@@ -81,6 +90,28 @@ def get_employee_one_name(db, name, controller_id):
     return db.query(Employee).filter(or_(Employee.firstname.ilike(f'%{name.lower()}%') , 
                                      Employee.lastname.ilike( f'%{name.lower()}%') &
                                      Employee.controller_id == controller_id)).all()
+    
+def edit_employee_image(db, id, firstname, lastname, image_path):
+    emp = db.query(Employee).filter(Employee.id == id).first()
+    if emp:
+        emp.firstname = firstname
+        emp.lastname = lastname
+        emp.image_path = image_path
+        db.commit()
+        return "Success"
+    else:
+        return "Failure"
+    
+def edit_employee_no_image(db, id, firstname, lastname):
+    emp = db.query(Employee).filter(Employee.id == id).first()
+    if emp:
+        emp.firstname = firstname
+        emp.lastname = lastname
+        db.commit()
+        return "Success"
+    else:
+        return "Failure"
+
 
 if __name__ == "__main__":
     init_db()
